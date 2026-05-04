b906375bdcf3926d11fe0c07c0fd4d1cbfc711a9
aqiliman <aqiliman24@gmail.com>
2026-05-04 16:54:38 +0800

    update module 6

--- module6_chat_memory/main.py
+++ module6_chat_memory/main.py
@@ -114,4 +114,4 @@ async def get_status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8004, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
